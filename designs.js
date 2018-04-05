904b89e7b68b47bdd6ee966921ff8aee98c60f0f
Sean J. McGehee <33711025+MobileWebGuy@users.noreply.github.com>
2018-04-05 13:06:34 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -5,6 +5,25 @@
 
 function makeGrid() {
 
-// Your code goes here!
+$(document).ready(function() {
+   $('#sizepicker').submit(function makeGrid(grid) {
+     $('table tr').remove();
+      var rows = $('#input_height').val();
+	  var cols = $('#input_width').val();
+	    for (var i = 1; i <= rows; i++) {
+		  $('table').append('<tr></tr>');
+		  for (var j = 1; j <= cols; j++) {
+		    $('tr:last').append('<td></td>');
+			S('td').attr("class", 'cells');
+		  }
+		}
+        grid.preventDefault();
+
+        $('.cells').click(function(event) {
+           var paint = $('#colorpicker').val();
+           $(event.target).css('background-color', paint);
+        });	
+     });	
+ });
 
 }
